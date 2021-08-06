80f94e4d3213d809f65d374595d22f19c36d2f82
Abu Saleh Md. Musa Sazib <musasazib@gmail.com>
2021-08-06 06:53:12 +0600

    Apply Function

---
+++ inchToFeet.js
@@ -0,0 +1,28 @@
+function inchToFeet(inches) {
+    var feet = inches / 12;
+    return feet;
+}
+
+var myInches = 132;
+var feet = inchToFeet(myInches);
+console.log(feet);
+
+var dadiInches = 144;
+var feet = inchToFeet(dadiInches);
+console.log(feet);
+
+var naniInches = 156;
+var feet = inchToFeet(naniInches);
+console.log(feet);
+
+var nanaInches = 168;
+var feet = inchToFeet(nanaInches);
+console.log(feet)
+
+var naniInches = 180;
+var feet = inchToFeet(naniInches);
+console.log(feet);
+
+var khalaInches = 192;
+var feet = inchToFeet(khalaInches);
+console.log(feet);
